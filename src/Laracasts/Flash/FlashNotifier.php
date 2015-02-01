5ee8dd8e723f63b1dd969b26bff16fb3099b68ca
Laracasts <jeffrey@laracasts.com>
2015-02-01 14:41:37 -0500

    Make fluent, and add "important" flag

--- src/Laracasts/Flash/FlashNotifier.php
+++ src/Laracasts/Flash/FlashNotifier.php
@@ -28,43 +28,55 @@ class FlashNotifier
     public function info($message)
     {
         $this->message($message, 'info');
+
+        return $this;
     }
 
     /**
      * Flash a success message.
      *
-     * @param string $message
+     * @param  string $message
+     * @return $this
      */
     public function success($message)
     {
         $this->message($message, 'success');
+
+        return $this;
     }
 
     /**
      * Flash an error message.
      *
-     * @param string $message
+     * @param  string $message
+     * @return $this
      */
     public function error($message)
     {
         $this->message($message, 'danger');
+
+        return $this;
     }
 
     /**
      * Flash a warning message.
      *
-     * @param string $message
+     * @param  string $message
+     * @return $this
      */
     public function warning($message)
     {
         $this->message($message, 'warning');
+
+        return $this;
     }
 
     /**
      * Flash an overlay modal.
      *
-     * @param string $message
-     * @param string $title
+     * @param  string $message
+     * @param  string $title
+     * @return $this
      */
     public function overlay($message, $title = 'Notice')
     {
@@ -72,18 +84,35 @@ class FlashNotifier
 
         $this->session->flash('flash_notification.overlay', true);
         $this->session->flash('flash_notification.title', $title);
+
+        return $this;
     }
 
     /**
      * Flash a general message.
      *
-     * @param string $message
-     * @param string $level
+     * @param  string $message
+     * @param  string $level
+     * @return $this
      */
     public function message($message, $level = 'info')
     {
         $this->session->flash('flash_notification.message', $message);
         $this->session->flash('flash_notification.level', $level);
+
+        return $this;
+    }
+
+    /**
+     * Add an "important" flash to the session.
+     *
+     * @return $this
+     */
+    public function important()
+    {
+        $this->session->flash('flash_notification.important', true);
+
+        return $this;
     }
 
 }
