e5cc8bda9d01de40efeb77c3ead769876deacded
Luciana is YammYcoding@freenode:6667 <cadastro@luciana.pro.br>
2015-03-07 20:45:37 -0300

    working fix for the important css class

--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -2,7 +2,7 @@
     @if (Session::has('flash_notification.overlay'))
         @include('flash::modal', ['modalClass' => 'flash-modal', 'title' => Session::get('flash_notification.title'), 'body' => Session::get('flash_notification.message')])
     @else
-        <div class="alert alert-{{ Session::get('flash_notification.level') }}">
+        <div class="alert alert-{{ Session::get('flash_notification.level') }} {{ Session::has( 'flash_notification.important' ) ? 'alert-important' : '' }}">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 
             {{ Session::get('flash_notification.message') }}
