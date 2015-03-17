0eb95683841976b47c0854f2018a38960833343d
Russ Weas <russweas@gmail.com>
2015-03-16 21:48:53 -0500

    Update message.blade.php
    If I say Session::message("<b>Success!</b> Your payment when through great.") now it will show the success in bold, and not escape the html.

--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -5,7 +5,7 @@
         <div class="alert alert-{{ Session::get('flash_notification.level') }}">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 
-            {{ Session::get('flash_notification.message') }}
+            {!! Session::get('flash_notification.message') !!}
         </div>
     @endif
 @endif
