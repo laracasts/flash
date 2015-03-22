3846b285595b8bdcc23ed4aba9baa678a975b48c
Shonetow <shonetow@gmail.com>
2015-03-22 06:28:14 +0100

    Changed curly brackets {{ }} => {!! !!}

--- src/views/modal.blade.php
+++ src/views/modal.blade.php
@@ -1,14 +1,14 @@
-<div id="flash-overlay-modal" class="modal fade {{ $modalClass or '' }}">
+<div id="flash-overlay-modal" class="modal fade {!! $modalClass or '' !!}">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 
-                <h4 class="modal-title">{{ $title }}</h4>
+                <h4 class="modal-title">{!! $title !!}</h4>
             </div>
 
             <div class="modal-body">
-                <p>{{ $body }}</p>
+                <p>{!! $body !!}</p>
             </div>
 
             <div class="modal-footer">
