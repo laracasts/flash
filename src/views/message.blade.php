0673a0e4fbb7db87d47fd0aa408f77c98e09dccf
tim-peterson <petersontimr@gmail.com>
2015-11-01 13:21:43 -0500

    updating

--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -9,22 +9,19 @@
       <div class="container-fluid">
         <div class="row">
           <div class="text-center">
-            {{ Session::get('flash_notification.message') }}
 
-                  <?php $flash_notification_messages = Session::get('flash_notification.message');
-                          $flash_notification_levels = Session::get('flash_notification.level');?>
-                    @foreach($flash_notification_messages as $index => $flash_notification_message)
-                    <div class="alert alert-{{ $flash_notification_levels[$index] }}"
-                        style="border-radius:0;border-left:0;border-right:0;">
-                        <button type="button" class="close"
-                          data-dismiss="alert" aria-hidden="true">
-                          <i class="lnr lnr-cross"></i>
-                        </button>
-                        {!! $flash_notification_message !!}
-                    </div>
-                    @endforeach
-
-
+              <?php $flash_notification_messages = Session::get('flash_notification.message');
+                      $flash_notification_levels = Session::get('flash_notification.level');?>
+                @foreach($flash_notification_messages as $index => $flash_notification_message)
+                  <div class="alert alert-{{ $flash_notification_levels[$index] }}"
+                      style="border-radius:0;border-left:0;border-right:0;">
+                      <button type="button" class="close"
+                        data-dismiss="alert" aria-hidden="true">
+                        <i class="lnr lnr-cross"></i>
+                      </button>
+                      {!! $flash_notification_message !!}
+                  </div>
+                @endforeach
 
           </div>
         </div>
