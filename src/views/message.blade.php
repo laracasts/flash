4df7200127a71511bdf116bc8b7a2586612c7a35
Jeffrey Way <jeffrey@jeffrey-way.com>
2016-03-14 11:55:34 -0400

    Clean up view

--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -1,14 +1,19 @@
-@if (Session::has('flash_notification.message'))
-    @if (Session::has('flash_notification.overlay'))
-        @include('flash::modal', ['modalClass' => 'flash-modal', 'title' => Session::get('flash_notification.title'), 'body' => Session::get('flash_notification.message')])
+@if (session()->has('flash_notification.message'))
+    @if (session()->has('flash_notification.overlay'))
+        @include('flash::modal', [
+            'modalClass' => 'flash-modal', 
+            'title'      => session('flash_notification.title'), 
+            'body'       => session('flash_notification.message')
+        ])
     @else
-        <div class="alert alert-{{ Session::get('flash_notification.level') }} {{ Session::has('flash_notification.important') ? 'alert-important' : '' }}">
-            @if(Session::has('flash_notification.important'))
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            @endif
-            {{ Session::get('flash_notification.message') }}
+        <div class="alert alert-{{ session('flash_notification.level') }}">
+            <button type="button" 
+                    class="close" 
+                    data-dismiss="alert" 
+                    aria-hidden="true">&times;</button>
+
+            {!! session('flash_notification.message') !!}
         </div>
     @endif
 @endif
+
