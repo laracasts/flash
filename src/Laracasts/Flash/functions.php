23a57218fb8dd0a56c46db272405bc9f50656ef1
Elvijs <elvijs.obuhovs@gmail.com>
2016-04-14 23:48:03 +0300

    Change alert levels in flash()
    Added posibility to change alert levels in flash() function

--- src/Laracasts/Flash/functions.php
+++ src/Laracasts/Flash/functions.php
@@ -8,12 +8,12 @@ if ( ! function_exists('flash')) {
      * @param  string|null $message
      * @return \Laracasts\Flash\FlashNotifier
      */
-    function flash($message = null)
+    function flash($message = null, $level = 'info')
     {
         $notifier = app('flash');
 
         if ( ! is_null($message)) {
-            return $notifier->info($message);
+            return $notifier->message($message, $level);
         }
 
         return $notifier;
