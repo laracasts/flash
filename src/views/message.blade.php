d9f6c8911e3e5bd827c485f9098e16e86aedddf2
Jeffrey Way <jeffrey@jeffrey-way.com>
2017-04-26 11:33:00 -0400

    Use shorthand syntax

--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -1,5 +1,5 @@
-@if (session()->has('flash_notification.message'))
-    @if (session()->has('flash_notification.overlay'))
+@if (session('flash_notification.message'))
+    @if (session('flash_notification.overlay'))
         @include('flash::modal', [
             'modalClass' => 'flash-modal',
             'title'      => session('flash_notification.title'),
@@ -8,13 +8,14 @@
     @else
         <div class="alert
                     alert-{{ session('flash_notification.level') }}
-                    {{ session()->has('flash_notification.important') ? 'alert-important' : '' }}"
+                    {{ session('flash_notification.important') ? 'alert-important' : '' }}"
         >
-            @if(session()->has('flash_notification.important'))
+            @if(session('flash_notification.important'))
                 <button type="button"
                         class="close"
                         data-dismiss="alert"
-                ><span aria-hidden="true">&times;</span></button>
+                        aria-hidden="true"
+                >&times;</button>
             @endif
 
             {!! session('flash_notification.message') !!}
