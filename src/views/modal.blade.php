96e8ebbf1a22c8bc58387ca5501685c4678d7055
Marnu Lombard <marnulombard@gmail.com>
2019-08-06 08:53:37 +0200

    [bugfix] Implement Laravel 5.7's dropping of `or` in blade templates - See https://laravel.com/docs/5.7/upgrade - When the `or` is evaluated now - it returns a boolean value - The resulting class is `"modal fade 1"` - Use `isset()` instead of null coalescence in order to maintain PHP `>=5.4.0` compatibility.

--- src/views/modal.blade.php
+++ src/views/modal.blade.php
@@ -1,4 +1,4 @@
-<div id="flash-overlay-modal" class="modal fade {{ $modalClass or '' }}">
+<div id="flash-overlay-modal" class="modal fade {{ isset($modalClass) ? $modalClass : '' }}">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
