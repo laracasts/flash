1acfbaec4e4658946de97897c878f59fdcc41b8e
Ronny-André Bendiksen <ronnyandre@gmail.com>
2019-10-15 14:00:53 +0200

    Localized label for close modal button

--- src/views/modal.blade.php
+++ src/views/modal.blade.php
@@ -12,7 +12,7 @@
             </div>
 
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">{{ __('Close') }}</button>
             </div>
         </div>
     </div>
